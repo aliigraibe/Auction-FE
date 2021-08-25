450504bc8f77b4d0a4391387428c47a5fbf5cc5f
basel1996-96 <baselabu4111@gmail.com>
2021-08-25 05:20:45 -0700

    design8

--- src/component/Auction/ActiveUsers.js
+++ src/component/Auction/ActiveUsers.js
@@ -1,9 +1,16 @@
-import { FlexStyleVer1 } from "../../styles";
 
+import pic18 from "../../images/pic18.png";
 const ActiveUsers = ({ roomUsers }) => {
   roomUsers = [...new Set(roomUsers)];
 
-  return <FlexStyleVer1>{roomUsers.length}</FlexStyleVer1>;
+  return <div>
+    <div className="eye2">
+    {roomUsers.length}
+    </div>
+    <div className="eye1">
+    <img className="eye" src={pic18} alt="go back" />
+    </div>
+    </div>;
 };
 
 export default ActiveUsers;
